057e2b27fc9f65f67765774b7080949ad78c2235
vitalix <vitalisbox@gmail.com>
2024-09-15 22:11:36 +0300

    +zsh

---
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -0,0 +1,14 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    go = { "gofmt", "goimports" },
+    html = { "prettier" },
+  },
+
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+require("conform").setup(options)
